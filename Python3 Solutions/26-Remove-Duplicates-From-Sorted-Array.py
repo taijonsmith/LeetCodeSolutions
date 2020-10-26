15f9fc0c17590504dec53b36e0d74ec7216969c8
TaiJon Smith <taijonsmith22@gmail.com>
2020-10-26 04:21:06 -0400

    Modified solution to remove duplicates problem, added comments

--- Python3 Solutions/26-Remove-Duplicates-From-Sorted-Array.py
+++ Python3 Solutions/26-Remove-Duplicates-From-Sorted-Array.py
@@ -2,17 +2,20 @@
 
 class Solution:
     def __init__(self): #value in x can be edited to test other solutions
-        nums = [1,1,2] #must be of type List[int]
+        nums = [0,0,1,1,1,2,2,3,3,4] #must be of type List[int]
         result = self.removeDuplicates(nums) #must be of type int
         print('Answer: ', result)
 
     def removeDuplicates(self, nums: [int]) -> int:
-        print(nums)
-        for index, n in enumerate(nums):
-            print(index)
-            if (index < len(nums) - 1 and n == nums[index + 1]):
-                nums.remove(n)
-        print(nums)
-        return len(nums)
+        if not nums:
+            return 0
+        p1 = 0 #p1 and p2 act as pointers for the indexes used during the sort
+        print('Before modification: ', nums)
+        for p2 in range(1, len(nums)):
+            if nums[p1] != nums[p2]:
+                p1 += 1
+                nums[p1] = nums[p2]
+        print('After modification: ', nums)
+        return p1 + 1 #returns number of distinct elements
 
 Solution()
